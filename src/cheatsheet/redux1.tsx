edb3c40b64106dccf7d8d1d1b8a4cfbfe11a0ec0
Pierrick Houée <houee.pierrick@gmail.com>
2022-10-28 12:41:59 +0200

    setting-up

---
+++ src/cheatsheet/redux1.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+
+// REDUX CODE
+///////////////////////////////////
+
+const increment = () => {
+  return { type: "increment" };
+};
+
+const decrement = () => {
+  return { type: "decrement" };
+};
+
+const initialState = 0;
+const counterReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(counterReducer);
+
+// REACT CODE
+///////////////////////////////////
+
+const render = () => {
+  ReactDOM.render(
+    <CounterApp state={store.getState()} />,
+    document.getElementById("root")
+  );
+};
+render();
+store.subscribe(render);
+
+function CounterApp(props) {
+  const state = props.state;
+
+  const onIncrementButtonClicked = () => {
+    store.dispatch(increment());
+  };
+
+  const onDecrementButtonClicked = () => {
+    store.dispatch(decrement());
+  };
+
+  return (
+    <div>
+      <h1> {state} </h1>
+      <button onClick={onIncrementButtonClicked}>+</button>
+      <button onClick={onDecrementButtonClicked}>-</button>
+    </div>
+  );
+}
